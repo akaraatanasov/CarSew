daa9cec13dbf84fe8ccd1ad9e78b42cefe0a9938
Alexander Karaatanasov <akaraatanasov@gmail.com>
2019-05-16 22:04:03 +0300

    Initial commit - UI done
    UI made entirely in Interface Builder (using Storyboards)

---
+++ carsew-ios/CarSew/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  CarSew
+//
+//  Created by Alexander Karaatanasov on 16.05.19.
+//  Copyright © 2019 Alexander Karaatanasov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
